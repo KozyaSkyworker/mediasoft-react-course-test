9572cb6bc68baeeb38dae013bd185f7bdf93043d
KozyaSkyworker <d_koz@inbox.ru>
2023-11-18 07:11:26 +0400

    install sass, styling, header

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import classes from './App.module.scss';
+import Header from './components/header/Header';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className={classes.wrapper}>
+      <Header />
+      <main className={classes.main}>2</main>
+      <footer className="footer">3</footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,67 @@
+import classes from './header.module.scss';
+import logo from './../../assets/logo.png';
+import { FaCartShopping } from 'react-icons/fa6';
+import { MdFavorite } from 'react-icons/md';
+import { IoMdSearch } from 'react-icons/io';
+
+const Header = () => {
+  return (
+    <header className={classes.header}>
+      <div className="container">
+        <div className={classes.header__top}>
+          <a className={classes.header__logo} href="/">
+            <img src={logo} alt="logo" height={20} />
+          </a>
+          <ul className={classes.header__list}>
+            <li className={classes.header__item}>
+              <input type="text" placeholder="Поиск" />
+              <IoMdSearch />
+            </li>
+            <li className={classes.header__item}>
+              <a href="/favorites">
+                Отложенные
+                <MdFavorite />
+              </a>
+            </li>
+
+            <li className={classes.header__item}>
+              <a href="/cart">
+                Корзина
+                <FaCartShopping />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className={classes.header__bot}>
+          <nav className={classes.header__navigation}>
+            <ul className={classes.header__list}>
+              <li className={classes.header__item}>
+                <a className={classes.header__link} href="/about">
+                  О нас
+                </a>
+              </li>
+              <li className={classes.header__item}>
+                <a className={classes.header__link} href="/paymontinfo">
+                  Оплата
+                </a>
+              </li>
+              <li className={classes.header__item}>
+                <a className={classes.header__link} href="/delivery">
+                  Доставка
+                </a>
+              </li>
+              <li className={classes.header__item}>
+                <a className={classes.header__link} href="/contacts">
+                  Контакты
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <span className="header__phone">+7(888) 888-88-88</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
